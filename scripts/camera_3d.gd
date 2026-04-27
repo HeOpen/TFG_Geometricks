82431e0f8d5802382a69e8c8177a3cc58d918c68
Espadas995 <joseespadas995@gmail.com>
2026-04-27 11:14:05 +0200

    Mejorando la iluminacion y el cambio de perspectiva
    Added: nuevas luces que cubran todo el cubo
    
    Updated: Bajado el nivel de luz ambiente para que se vean mejor las sombras
    
    Updated: el cambio de perspectiva ahora es transicional, mediante una velocidad ajustable, hay algun glitch de pantallazo negro al cambiar de perspectiva

--- scripts/camera_3d.gd
+++ scripts/camera_3d.gd
@@ -2,26 +2,32 @@ extends Camera3D
 
 var target: Node3D
 @export var distance: float = 10.0
+@export var rotation_speed: float = 8.5
+
 var orientation: Basis
+var target_orientation: Basis
 
 func _ready() -> void:
 	target = get_parent()
 	global_position = target.global_position + Vector3(0, 0, distance)
 	look_at(target.global_position, Vector3.UP)
 	orientation = global_transform.basis
+	target_orientation = orientation
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("cam_left"):
-		orientation = Basis(orientation.y.normalized(), deg_to_rad(-90)) * orientation
+		target_orientation = Basis(target_orientation.y.normalized(), deg_to_rad(-90)) * target_orientation
 	if Input.is_action_just_pressed("cam_right"):
-		orientation = Basis(orientation.y.normalized(), deg_to_rad(90)) * orientation
+		target_orientation = Basis(target_orientation.y.normalized(), deg_to_rad(90)) * target_orientation
 	if Input.is_action_just_pressed("cam_up"):
-		orientation = Basis(orientation.x.normalized(), deg_to_rad(-90)) * orientation
+		target_orientation = Basis(target_orientation.x.normalized(), deg_to_rad(-90)) * target_orientation
 	if Input.is_action_just_pressed("cam_down"):
-		orientation = Basis(orientation.x.normalized(), deg_to_rad(90)) * orientation
+		target_orientation = Basis(target_orientation.x.normalized(), deg_to_rad(90)) * target_orientation
 	if Input.is_action_just_pressed("cam_roll_left"):
-		orientation = Basis(orientation.z.normalized(), deg_to_rad(-90)) * orientation
+		target_orientation = Basis(target_orientation.z.normalized(), deg_to_rad(-90)) * target_orientation
 	if Input.is_action_just_pressed("cam_roll_right"):
-		orientation = Basis(orientation.z.normalized(), deg_to_rad(90)) * orientation
-	global_position = target.global_position + orientation.z * distance
-	global_transform.basis = orientation
+		target_orientation = Basis(target_orientation.z.normalized(), deg_to_rad(90)) * target_orientation
+
+	orientation = orientation.slerp(target_orientation, rotation_speed * delta)
+
+	global_transform = Transform3D(orientation, target.global_position + orientation.z * distance)
