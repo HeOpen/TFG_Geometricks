a4b7dc4847c4526a4d8f27d2c8b05c5261d40957
HeOpen <00eliabeed1@gmail.com>
2026-05-19 23:46:46 +0200

    Added: sello de avingnon (pentagrama) y sotano como sala ritualistica

---
+++ objects/props/cubo.gd
@@ -0,0 +1,10 @@
+extends CSGBox3D
+
+@export var x_speed:float = 1.5
+@export var y_speed:float = 2
+@export var z_speed:float = 2
+
+func _process(delta):
+	rotate_x(x_speed * delta)
+	rotate_y(y_speed * delta)
+	rotate_z(z_speed * delta)
