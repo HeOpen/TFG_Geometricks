6bab8a98757719d41d0b87194cb0b6cf9ddaa32b
Espadas <joseespadas995@gmail.com>
2026-05-22 10:59:24 +0200

    Implementación de cambio de cara en cubo 2D

---
+++ scripts_2d/cara_base.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+signal player_exiting(side: String, player_y: float, player_vel: Vector2)
+
+const VIEWPORT_SIZE := 512.0
+const EXIT_THRESHOLD := 8.0
+
+var _exit_pending := false
+
+func _physics_process(_delta: float) -> void:
+	var player: CharacterBody2D = get_node_or_null("bola_2d")
+	if not player:
+		_exit_pending = false
+		return
+	if _exit_pending:
+		return
+
+	if player.position.x >= VIEWPORT_SIZE - EXIT_THRESHOLD:
+		_exit_pending = true
+		player_exiting.emit("right", player.position.y, player.velocity)
+	elif player.position.x <= EXIT_THRESHOLD:
+		_exit_pending = true
+		player_exiting.emit("left", player.position.y, player.velocity)
