ff9d630936980c048ee119cf759cd46de10342c5
HeOpen <00eliabeed1@gmail.com>
2026-05-24 18:55:22 +0200

    Updated: Menu, intro y nivel 1 linkeados funcionando.
    Added: Sistema de inventario e interacción con escenario intro

---
+++ level/3d_cabin/gestor_intro.gd
@@ -0,0 +1,44 @@
+extends Node3D
+
+# --- Referencias Expuestas ---
+# Asigna estos nodos desde el panel Inspector del editor
+@export var audio_noticias: AudioStreamPlayer3D
+@export var cinta_vhs: StaticBody3D
+
+func _ready() -> void:
+	# 1. Fase de Preparación Cero
+	_ocultar_cinta_inicialmente()
+	
+	# 2. Arranque de la máquina de estados temporal
+	_iniciar_secuencia()
+
+func _ocultar_cinta_inicialmente() -> void:
+	if cinta_vhs:
+		# Hacemos la malla invisible
+		cinta_vhs.visible = false
+		# Desactivamos el procesamiento físico y lógico. 
+		# Esto asegura que el RayCast3D del jugador ignore el objeto por completo.
+		cinta_vhs.process_mode = Node.PROCESS_MODE_DISABLED
+	else:
+		push_error("Error: Nodo cinta_vhs no asignado en GestorIntro.")
+
+func _iniciar_secuencia() -> void:
+	# Retención 1: Silencio inicial
+	# create_timer genera un nodo temporal en memoria que emite la señal 'timeout' tras X segundos
+	# await detiene el avance de esta función hasta recibir dicha señal
+	await get_tree().create_timer(5.0).timeout
+	
+	if audio_noticias:
+		audio_noticias.play()
+	
+	# Retención 2: Transmisión de las noticias
+	await get_tree().create_timer(50.0).timeout
+	
+	_manifestar_cinta_vhs()
+
+func _manifestar_cinta_vhs() -> void:
+	if cinta_vhs:
+		# Restauramos la visibilidad
+		cinta_vhs.visible = true
+		# Devolvemos el estado de colisiones a la normalidad heredada
+		cinta_vhs.process_mode = Node.PROCESS_MODE_INHERIT
